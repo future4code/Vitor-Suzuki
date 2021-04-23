53f15031b6eb54d632e54d72962815fd86d18c20
Vitor Suzuki <vitor.szk@outlook.com>
2021-04-23 14:26:49 -0300

    Exercícios da aula 13 resolvidos.

---
+++ semana3/loops/index.js
@@ -0,0 +1,75 @@
+//          ---------- EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ----------
+
+  // EXERCÍCIO 1:
+
+      // Este código está fazendo um loop, onde a variável "i" deve assumir valor unitário a cada ciclo, iniciando-se do 0 e terminando em 5. A variável "valor" irá somar todos os
+      //valores "i", até ir ser igual à 5. 
+
+
+  // EXERCÍCIO 2: 
+
+      // a. No console, será impresso todos os valores do array acima de 18. 
+      // b. O for of é suficiente, sendo necessário apenas usar sintaxe de desestruturação, .entries().
+
+  // DESAFIO:
+
+//          ---------- EXERCÍCIOS DE ESCRITA DE CÓDIGO ----------
+ 
+
+  // EXERCÍCIO 3:
+
+    // a.  const arrayOriginal = [10, 30, 60, 70, 80]
+  
+    //     for(let i = 0; i < arrayOriginal.length; i++) {
+    //         console.log(arrayOriginal[i])
+    //     }
+
+
+  
+    // b.  const arrayOriginal = [10, 30, 60, 70, 80]
+  
+    //     for(let i = 0; i < arrayOriginal.length; i++) {
+    //         console.log(arrayOriginal[i] / 10)
+    //     }   
+    
+    
+
+    // c.  const arrayOriginal = [10, 30, 60, 70, 80]
+  
+    //     for(let i = 0; i < arrayOriginal.length; i++) {
+    //         if((i % 2) == 0) {
+    //             console.log(arrayOriginal[i])
+    //         }
+            
+    //     }   
+
+
+    // d. const arrayOriginal = [10, 30, 60, 70, 80]
+
+    //   for(const index of arrayOriginal.entries()){
+    //       console.log(`O elemento do índex ${index[0]} é ${index[1]}`)
+    //   }
+
+
+
+
+    // e. let arrayOriginal = [10, 30, 60, 70, 80]
+
+    //    let maiorNumero = arrayOriginal[0];
+    //    let menorNumero = arrayOriginal[4];
+    
+    //    for (let i = 0; i < arrayOriginal.length; i++) {
+    //        if (arrayOriginal[i] >= maiorNumero) {
+    //            maiorNumero = arrayOriginal[i];
+    //        }
+    //    }
+
+    //    console.log(maiorNumero)
+
+    //    for (let i = 0; i < arrayOriginal.length; i++) {
+    //        if (arrayOriginal[i] <= menorNumero) {
+    //            menorNumero = arrayOriginal[i];
+    //        }
+    //    }    
+
+    //    console.log(menorNumero)
