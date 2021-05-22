b7771cb4287d7ed0bfad5714a396ab48eb224820
Vitor Suzuki <vitor.szk@outlook.com>
2021-05-22 19:52:56 -0300

    Funções finalizadas

--- semana7/semana7-labefy/projeto-labefy/src/App.js
+++ semana7/semana7-labefy/projeto-labefy/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
+import imagem from './img/red-play-button.png'
 
 export default class App extends React.Component {
   state = {
@@ -8,11 +9,53 @@ export default class App extends React.Component {
     inputName: "",
     inputSearch: "",
     resultSearch: [],
-    playlistTrack: []
+    playlistTrack: [],
+    inputNameTrack: "",
+    inputArtistTrack: "",
+    inputUrlTrack: "",
+    select: "",
+    sourceUrl: "",
+    loading: false
   }
 
   componentDidMount() {
     this.getAllPlaylists()
+  }
+
+
+
+  handleName = (e) => {
+    this.setState({ inputName: e.target.value })
+  }
+
+  handleClickEnter = (e) => {
+    if (e.keyCode === 13) {
+      this.createPlaylist()
+    }
+  }
+
+  handleSearchEnter = (e) => {
+    if (e.keyCode === 13) {
+      this.searchPlaylists()
+    }
+  }
+
+  handleSearch = (e) => {
+    this.setState({ inputSearch: e.target.value })
+  };
+
+  handleNameTrack = (e) => {
+    this.setState({ inputNameTrack: e.target.value })
+  }
+  handleArtistTrack = (e) => {
+    this.setState({ inputArtistTrack: e.target.value })
+  }
+  handleUrlTrack = (e) => {
+    this.setState({ inputUrlTrack: e.target.value })
+  }
+
+  handleSelect = (e) => {
+    this.setState({ select: e.target.value })
   }
 
   getAllPlaylists = () => {
@@ -30,26 +73,6 @@ export default class App extends React.Component {
         console.log(err.response.data)
       })
   }
-
-  handleName = (e) => {
-    this.setState({ inputName: e.target.value })
-  }
-
-  handleClickEnter = (e) => {
-    if (e.keyCode === 13) {
-      this.createPlaylist()
-    }
-  }
-
-  handleSearchEnter = (e) => {
-    if (e.keyCode === 13) {
-      this.searchPlaylists  ()
-    }
-  }
-
-  handleSearch = (e) => {
-    this.setState({ inputSearch: e.target.value })
-  };
 
   createPlaylist = () => {
     const header = {
@@ -84,6 +107,7 @@ export default class App extends React.Component {
 
     axios.get(url, header)
       .then((res) => {
+        console.log(res)
         this.setState({ resultSearch: res.data.result.playlist })
         this.setState({ inputSearch: "" })
       })
@@ -91,18 +115,6 @@ export default class App extends React.Component {
         console.log(err.response)
       })
 
-  }
-
-  renderPlaylistComponents = () => {
-    return this.state.playlists.map((pl) => {
-      return <li key={pl.id}>{pl.name}</li>
-    })
-  }
-
-  renderResultSearch = () => {
-    return this.state.resultSearch.map((playlist) => {
-      return <li key={playlist.id} onClick={() => this.getPlaylistTracks(playlist.id)}>{playlist.name}</li>
-    })
   }
 
   getPlaylistTracks = (id) => {
@@ -114,13 +126,63 @@ export default class App extends React.Component {
     const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
 
     axios.get(url, header)
-    .then((res) => {
-      console.log(res.result)
-    })
-    .catch((err) => {
-      this.setState({})
+      .then((res) => {
+        this.setState({ playlistTrack: res.data.result.tracks })
+      })
+      .catch((err) => {
+        console.log(err.response)
+      })
+  }
+
+  addTrackToPlaylist = () => {
+    const header = {
+      headers: {
+        Authorization: "vitor-suzuki-paiva"
+      }
+    }
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.state.select}/tracks`
+    const body = {
+      "name": this.state.inputNameTrack,
+      "artist": this.state.inputArtistTrack,
+      "url": this.state.inputUrlTrack
+    }
+    axios.post(url, body, header)
+      .then(() => {
+        alert("Música adicionada!")
+      })
+      .catch(() => {
+        alert("Tente novamente :(")
+      })
+
+  }
+
+  clickPlay = async (url) => {
+    this.setState({loading: true})
+    await this.setState({sourceUrl: url})
+    this.setState({loading: false})
+  }
+
+  renderPlaylistComponents = () => {
+    return this.state.playlists.map((pl) => {
+      return <li key={pl.id}>{pl.name}</li>
     })
   }
+
+  renderResultSearch = () => {
+    return this.state.resultSearch.map((playlist) => {
+      return <li className="track" key={playlist.id} onClick={() => this.getPlaylistTracks(playlist.id)}>{playlist.name}</li>
+    })
+  }
+
+  renderPlaylistTracks = () => {
+    return this.state.playlistTrack.map((track) => {
+      return <><p key={track.id}>{track.name}</p>
+      <img src={imagem} onClick={() => this.clickPlay(track.url)}></img></>
+      
+    })
+  }
+
+
 
   render() {
     return (
@@ -143,6 +205,23 @@ export default class App extends React.Component {
           <input value={this.state.inputSearch} onChange={this.handleSearch} onKeyDown={this.handleSearchEnter}></input>
           <button onClick={this.searchPlaylists}>Procurar</button>
           {this.renderResultSearch()}
+          {this.renderPlaylistTracks()}
+          {this.state.playlistTrack.length > 0 && !this.state.loading && <audio controls>
+            <source src={this.state.sourceUrl} type="audio/mpeg"></source>
+          </audio>}
+          <button onClick={() => console.log(this.state)}>Teste</button>
+          <hr />
+          <h2>Adicionar Músicas</h2>
+          <select onChange={this.handleSelect}>
+            <option>Selecione a playlist</option>
+            {this.state.playlists.map((playlist) => {
+              return <option value={playlist.id}>{playlist.name}</option>
+            })}
+          </select>
+          <input value={this.state.inputNameTrack} onChange={this.handleNameTrack} placeholder="Nome da música"></input>
+          <input value={this.state.inputArtistTrack} onChange={this.handleArtistTrack} placeholder="Nome do artista"></input>
+          <input value={this.state.inputUrlTrack} onChange={this.handleUrlTrack} placeholder="Url da música"></input>
+          <button onClick={this.addTrackToPlaylist}>Adicionar</button>
         </div>
       </div>
     );
