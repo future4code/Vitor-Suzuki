619748146a16f854e348a71fc4f42b10aabd3c40
Vitor Suzuki <vitor.szk@outlook.com>
2021-06-04 16:58:08 -0300

    Astromatch finalizado

--- semana9/astromatch/src/components/Home/Home.js
+++ semana9/astromatch/src/components/Home/Home.js
@@ -1,22 +1,91 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios"
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import styled from 'styled-components';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+// ------- ESTILIZAÇÃO -------- // 
+
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    padding: 10px;
+
+`
+
+const CardWrap = styled.div`
+    padding: 20px 10px;
+`
+
+const ButtonMatches = styled.div`
+    display: flex;
+    justify-content: center;
+`
+const ButtonSwipe = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const ImageWrap = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const Image = styled.img`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
+    padding: 10px;
+`
+const ButtonHeart = styled.div`
+    &:hover {
+        cursor: pointer;
+        color: green;
+    }
+`
+const ButtonX = styled.div`
+    &:hover {
+        cursor: pointer;
+        color: red;
+    }
+`
+const Loading = styled.div`
+    position: absolute;
+    top: 50vh;
+    left: 50vw;
+`
+
+// ------- FUNCIONALIDADES -------- // 
 
 const Home = (props) => {
 
     const [profile, setProfile] = useState({})
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         getProfileToChoose()
     }, [])
 
     const getProfileToChoose = () => {
+        setLoading(true)
         axios
             .get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/vitor-suzuki/person`)
-            .then((res) => setProfile(res.data.profile))
+            .then((res) => {
+                setProfile(res.data.profile)
+                setLoading(false)
+            })
             .catch(() => alert("Acabaram os perfis :c"))
     };
 
     const postChoosePerson = (choice) => {
+        setLoading(true)
         const header = {
             headers: {
                 "Content-Type": "application/json"
@@ -31,9 +100,12 @@ const Home = (props) => {
 
         axios
             .post(url, body, header)
-            .then(() => { getProfileToChoose() })
-            .catch(() => { alert("Acabaram os perfis :c") })
+            .then(() => {
+                getProfileToChoose()
+                setLoading(false)
+            })
 
+            .catch(() => { alert("Acabaram os perfis :c") })
 
     }
 
@@ -47,25 +119,56 @@ const Home = (props) => {
             "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/vitor-suzuki/clear"
 
         axios
-        .put(url, header)
-        .then(() => {alert("Matches limpados!")})
-        .catch((err) => {console.log(err)})
+            .put(url, header)
+            .then(() => {
+                alert("A lista de Matches foi resetada!")
+                getProfileToChoose()
+            })
+            .catch((err) => { console.log(err) })
+
     }
 
     return (
         <div>
-            <button onClick={() => props.changePage()}>Matches</button>
-            { profile ?
-                <div>
-                    <h1>{profile.name}</h1>
-                    <h3>{profile.age}</h3>
-                    <p>{profile.bio}</p>
-                </div>
-                :
-                <button onClick={() => clearMatches()}>Clear Matches!</button>
-            }
-            <button onClick={() => postChoosePerson(false)}>No Match</button>
-            <button onClick={() => postChoosePerson(true)}>Match</button>
+            {loading ? <Loading><CircularProgress /></Loading> :
+                <CardContainer>
+
+                    <Card>
+                        <CardWrap>
+                            {profile ?
+                                <div>
+                                    <ButtonMatches>
+                                        <Button variant="outlined" onClick={() => props.changePage()}>Matches</Button>
+                                    </ButtonMatches>
+                                    <ImageWrap>
+                                        <Image src={profile.photo}></Image>
+                                    </ImageWrap>
+                                    <h1>{profile.name}</h1>
+                                    <h3>Idade: {profile.age}</h3>
+                                    <p>{profile.bio}</p>
+                                    <ButtonSwipe>
+                                        <ButtonX>
+                                            <ClearOutlinedIcon onClick={() => postChoosePerson(false)}></ClearOutlinedIcon>
+                                        </ButtonX>
+                                        <ButtonHeart>
+                                            <FavoriteIcon onClick={() => postChoosePerson(true)}></FavoriteIcon>
+                                        </ButtonHeart>
+                                    </ButtonSwipe>
+                                </div>
+                                :
+                                <div>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        startIcon={<DeleteIcon />}
+                                        onClick={() => clearMatches()}>Limpar Lista de Matches!
+                                    </Button>
+                                </div>
+                            }
+
+                        </CardWrap>
+                    </Card>
+                </CardContainer>}
         </div>
     );
 }
--- semana9/astromatch/src/components/Matches/Matches.js
+++ semana9/astromatch/src/components/Matches/Matches.js
@@ -1,5 +1,31 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
+import Card from '@material-ui/core/Card';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+
+// ------- ESTILIZAÇÃO -------- // 
+
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    padding: 10px;
+
+`
+
+const CardWrap = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: column;
+    padding: 20px 10px;
+`
+
+// ------- FUNCIONALIDADES -------- // 
 
 const Matches = (props) => {
 
@@ -20,16 +46,20 @@ const Matches = (props) => {
 
     return matches.map((match) => {
       return (<div>
-                <h3>{match.name}</h3> <h5>{match.age}</h5>
-              </div>)
+        <h5>{match.name}</h5>
+      </div>)
     })
   }
 
   return (
-    <div>
-      <button onClick={() => props.changePage()}>Astromatch</button>
-      {renderMatches()}
-    </div>
+    <CardContainer>
+      <Card>
+        <CardWrap>
+          <Button varitan="outlined" onClick={() => props.changePage()}>Astromatch</Button>
+          {renderMatches()}
+        </CardWrap>
+      </Card>
+    </CardContainer>
   );
 }
 
