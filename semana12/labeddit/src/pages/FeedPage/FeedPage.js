c3bcb4512bec001bd3063cfac0d9a9af7920e382
Vitor Suzuki <vitor.szk@outlook.com>
2021-06-24 19:46:54 -0300

    Páginad de feed criada, sem funcionalidades

--- semana12/labeddit/src/pages/FeedPage/FeedPage.js
+++ semana12/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useProtectedPage from '../../hooks/useProtectedPage';
+import PostCard from '../../components/PostCard/PostCard';
+import { BASE_URL } from '../../constants/urls';
+import useRequestData from '../../hooks/useRequestData';
 
 
 const FeedPage = () => {
     useProtectedPage()
+
+    const posts = useRequestData([], `${BASE_URL}/posts`)
+
+    const postFeed = posts.map((post) => {
+        return (
+            <PostCard
+                title={post.title}
+                body={post.body}
+                createdAt={post.createdAt}
+                voteSum={post.voteSum}
+                userVote={post.userVote}
+                commentCount={post.commentCount}
+                username={post.username}
+
+                onClick={() => null}
+            />
+        )
+    })
+
     return (
         <div>
-            <h1>Página de Feed</h1>
+            {postFeed}
         </div>
     )
 }
