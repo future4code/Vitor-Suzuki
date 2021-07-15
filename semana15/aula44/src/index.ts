d36d856557d112a2f9c253fddfe3df0f8d1603f0
Vitor Suzuki <vitor.szk@outlook.com>
2021-07-15 19:29:33 -0300

    Exercicios 1, 2 e 3 da aula 44

--- semana15/aula44/src/index.ts
+++ semana15/aula44/src/index.ts
@@ -9,7 +9,6 @@ type User = {
   age: number
 }
 
-// Mock simulando um array de usuários no Banco de Dados
 let users: User[] = [
   {
       id: 1,
@@ -55,9 +54,61 @@ let users: User[] = [
   }
 ]
 
+
+enum USER_ROLES {
+  ADMIN = "ADMIN",
+  NORMAL = "NORMAL"
+}
+
+
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+
+// -- EXERCÍCIO 1  a) Utiliza-se o método GET b) A entidade é indicada por '/users'  -- //
+app.get('/users', (req: Request, res: Response) => {
+    const result = users.map((user: User) => {
+      return { id: user.id, name: user.name, email: user.email, type: user.type, age: user.age}
+    });
+    res.status(200).send(result)
+}) 
+
+// -- EXERCÍCIO 2  --  a) Os parâmetros foram passados via query params, pois nõa é obrigatório.//
+app.get("/users", (req: Request, res: Response) => {
+  try {
+    let type = req.query.type as string
+    let search = req.query.search as string
+
+    if (type) {
+      type = type.toUpperCase()
+
+      if (type in USER_ROLES) {
+        const result = users.filter(user => user.type === type)
+        return res.status(200).send(result)
+      }
+    }
+
+    if (search) {
+      search = search.toLowerCase()
+
+      const result = users.filter(
+        user => user.name.toLowerCase().includes(search)
+      )
+
+      if (result.length === 0) {
+        return res.status(204).send("No user was found")
+      }
+      
+      return res.status(200).send(result)
+    }
+
+    res.status(200).send(users)
+  } catch (err) {
+    res.status(400).send("Error")
+  }
+})
+
 
 // Para testar se o servidor está tratando os endpoints corretamente
 app.get("/ping", (req: Request, res: Response) => {
