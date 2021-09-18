7f1230277494928ede0018065352eda919068cba
Vitor Suzuki <vitor.szk@outlook.com>
2021-09-18 13:52:44 -0300

    Rotas criadas

---
+++ semana24/promobit-frontend/src/routes/router.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import HomePage from "../pages/HomePage"
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+                <Route exact path="/detail/:id">
+                    <HomePage />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router 
