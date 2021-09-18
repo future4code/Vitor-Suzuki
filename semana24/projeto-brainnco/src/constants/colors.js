6f61f6656e36da8afba47f694a8c2de23f5159ac
Vitor Suzuki <vitor.szk@outlook.com>
2021-09-18 17:08:06 -0300

    Mudança de projeto.

---
+++ semana24/projeto-brainnco/src/constants/colors.js
@@ -0,0 +1,2 @@
+export const primaryColor = "#0f141a"
+export const secondaryColor = "#32c1ad" 
--- semana24/promobit-frontend/src/routes/coordinator.js
+++
@@ -1,7 +0,0 @@
-export const goToHomePage = (history) => {
-    history.push("/")
-} 
-
-export const goToDetailPage = (history) => {
-    history.push("/detail/:id")
-} 
